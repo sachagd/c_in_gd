24711aa3af8b70b57f1ba75583ffa9719e26df3a
Sacha <sach4gd@gmail.com>
2025-04-05 16:42:00 +0200

    edit code.c

--- code.c
+++ code.c
@@ -1,22 +1,4 @@
-#include <stdbool.h>
-#include <stdio.h>
-
 int main(){
-    int t[30];
-    int i = 0;
-    int k = 2;
-    while(i < 30){
-        bool c = true;
-        for (int j = 0; j < i; j++){
-            if (k%t[j] == 0){
-                c = false;
-            }
-        } 
-        if (c){
-            t[i] = k;
-            i++;
-        }
-        k++;
-    }
+    //write code
     return 0;
 }
